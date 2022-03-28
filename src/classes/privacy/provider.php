d170822250c0da227bbff47bdb58be6ad4ee1ea9
Gustavo Bazzo <gcbazzo@gmail.com>
2022-03-28 13:28:22 -0400

    Added namespace

--- src/classes/privacy/provider.php
+++ src/classes/privacy/provider.php
@@ -14,24 +14,12 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-/**
- * Privacy Subsystem implementation for tool_phpunit.
- *
- * @package    tool_phpunit
- * @copyright  2018 Mark Nelson <markn@moodle.com>
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
 
-namespace tool_phpunit\privacy;
+
+namespace tool_recitmigrator\privacy;
 
 defined('MOODLE_INTERNAL') || die();
 
-/**
- * Privacy Subsystem for tool_phpunit implementing null_provider.
- *
- * @copyright  2018 Mark Nelson <markn@moodle.com>
- * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- */
 class provider implements \core_privacy\local\metadata\null_provider {
 
     /**
